e36846f1ad3ca16f363ff04b9e079bc210c44ad8
YAZAMIT1715 <gtaop199@gmail.com>
2023-03-17 16:23:00 -0700

    hello

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-		char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
-			char Alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-				int i;
-					int j;
+char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+char Alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+int j;
 
-						for (i = 0; i < 26; i++)
-								{
-											putchar(alphabets[i]);
-												}
-							for (j = 0; j < 26; j++)
-									{
-												putchar(Alphabets[j]);
-													}
-									putchar('\n');
-										return (0);
+for (i = 0; i < 26; i++)
+{
+putchar(alphabets[i]);
+}
+for (j = 0; j < 26; j++)
+{
+putchar(Alphabets[j]);
+}
+putchar('\n');
+return (0);
 }
